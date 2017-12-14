7b06067ae30e49fef1c0173d198e192c5ff4741c
lhancquart <louis.hancquart@lyra-network.co.in>
2017-12-14 10:41:43 +0530

    1st commit

---
+++ app/src/main/java/in/co/lyra_network/android_java_lyra_inappdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package in.co.lyra_network.android_java_lyra_inappdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
